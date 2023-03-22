373f3ef91e2dbc2995c74cb40bfe127674334d9b
Luan <savarizluan@gmail.com>
2023-03-21 23:30:11 -0300

    Fix main

--- gaules/main.py
+++ gaules/main.py
@@ -2,18 +2,26 @@ import time
 import datetime
 from schedule import repeat, every, run_pending
 from ingestor import Ingestor
+import logging
+
+#Initializing logger
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     gaules = Ingestor("gaules")
 
     @repeat(every(60).seconds)
     def job():
-        gaules.ingest()
 
-    start_time = datetime.datetime(2023, 3, 22, 0, 0, 0, 0)
-    end_time = start_time + datetime.timedelta(days = 7)
+        start_time = datetime.datetime(2023, 3, 21, 0, 0, 0)
+        end_time = start_time + datetime.timedelta(days = 7)
+
+        if datetime.datetime.now() >= start_time and datetime.datetime.now() <= end_time:
+            gaules.ingest()
+        else:
+            logger.info(f"Run completed. Nothing else to do here.")
 
     while True:
-        if datetime.datetime.now() >= start_time and datetime.datetime.now() <= end_time:
-            run_pending()
-            time.sleep(0.5)
+        run_pending()
+        time.sleep(1)
