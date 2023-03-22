89da09905b60fa96a5d5d86647271a9c34837e88
Luan <savarizluan@gmail.com>
2023-03-21 22:52:35 -0300

    Fix main

--- gaules/main.py
+++ gaules/main.py
@@ -1,16 +1,19 @@
 import time
-
+import datetime
 from schedule import repeat, every, run_pending
 from ingestor import Ingestor
 
 if __name__ == "__main__":
     gaules = Ingestor("gaules")
 
-
-    @repeat(every(10).seconds)
+    @repeat(every(60).seconds)
     def job():
         gaules.ingest()
 
+    start_time = datetime.datetime(2023, 3, 22, 0, 0, 0, 0)
+    end_time = start_time + datetime.timedelta(days = 7)
+
     while True:
-        run_pending()
-        time.sleep(0.5)
+        if datetime.datetime.now() >= start_time and datetime.datetime.now() <= end_time:
+            run_pending()
+            time.sleep(0.5)
